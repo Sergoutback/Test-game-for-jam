b1ae2dbf7d9225ea8336a503cbe5cb0df5f4df9e
SergoutbackDesktop <webart2013@gmail.com>
2022-04-24 22:38:48 +0300

    24042022_2238
    fix player movements

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -6,36 +6,35 @@ using UnityEngine;
 public class PlayerMoves : MonoBehaviour
 {
     public GameObject player;
-    public int speed = 5;
-    public int speedRotation = 3;
+    public int speed = 300;
+    public int speedRotation = 2;
 
     void Start()
     {
         player = (GameObject)this.gameObject;
     }
 
+    [System.Obsolete]
     void Update()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            player.transform.position += player.transform.forward * speed * Time.deltaTime;
+            player.transform.position += player.transform.up * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            player.transform.position -= player.transform.forward * speed * Time.deltaTime;
+            player.transform.position -= player.transform.up * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            player.transform.Rotate(Vector3.down * speedRotation);
+            player.transform.Rotate(Vector3.fwd * speedRotation);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            player.transform.Rotate(Vector3.up * speedRotation);
+            player.transform.Rotate(Vector3.fwd * -speedRotation);
         }
     }
-
-    
 
 }
