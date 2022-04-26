d2b71386909d469128142e2a109b606b9a808aa6
Dmitry <dimaskarakozov@gmail.com>
2022-04-26 12:28:22 +0300

    Shadows & FixBug

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -9,8 +9,11 @@ public class PlayerMoves : MonoBehaviour
     public int speed = 300;
     public int speedRotation = 2;
 
+    private Animator anim;
+
     void Start()
     {
+        anim = gameObject.GetComponent<Animator>();
         player = (GameObject)this.gameObject;
     }
 
@@ -20,20 +23,26 @@ public class PlayerMoves : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             player.transform.position += player.transform.up * speed * Time.deltaTime;
+            anim.SetInteger("Pose", 1);
         }
 
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             player.transform.position -= player.transform.up * speed * Time.deltaTime;
+            anim.SetInteger("Pose", 0);
         }
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             player.transform.position -= player.transform.right * speed * Time.deltaTime;
+            anim.SetInteger("Pose", 2);
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             player.transform.position += player.transform.right * speed * Time.deltaTime;
+            anim.SetInteger("Pose", 2);
+            gameObject.transform.localScale = new Vector3(1,1,1);
         }
     }
 
---
+++ Assets/Scripts/camcontroll.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class camcontroll : MonoBehaviour {
+	public Transform playerTransform;
+	public float moveSpeed;
+	
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		this.transform.position = new Vector3(){
+				x=playerTransform.position.x,
+				y=playerTransform.position.y,
+				z=transform.position.z,
+		};
+		if(this.playerTransform){
+		Vector3 target= new Vector3(){
+		x=playerTransform.position.x,
+		y=playerTransform.position.y,
+		z=transform.position.z,
+		};
+	*/
+		Vector3 target = new Vector3()
+		{
+			x = playerTransform.position.x,
+			y = playerTransform.position.y,
+			z = transform.position.z,
+		};
+			Vector3 pos= Vector3.Lerp(a: transform.position,b: target, t: moveSpeed * Time.deltaTime);
+		transform.position = pos;
+		
+		
+
+	}
+}
