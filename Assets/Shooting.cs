328268e29126debaf12953b37d5e77f4d50ec601
Zahir9910 <64544761+Zahir9910@users.noreply.github.com>
2022-04-27 15:08:33 +0300

    Lich Shooting
    Lich Shooting

---
+++ Assets/Shooting.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firePoint;
+    public float bulletSpeed = 50;
+    public GameObject Other;
+    Vector2 lookDirection;
+    float lookAngle;
+
+    void Update()
+    {
+        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+        firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletClone = Instantiate(bullet);
+            bulletClone.transform.position = firePoint.position;
+            bulletClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle);
+
+            bulletClone.GetComponent<Rigidbody2D>().velocity = firePoint.right * bulletSpeed;
+        }
+
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+    void OnCollisionEnter2D(Collision2D shit)
+    {
+        if (shit.gameObject.tag == "Player")
+        {
+            Destroy();
+        }
+        if (shit.gameObject.tag == "Ground")
+        {
+            Destroy();
+        }
+        if (shit.gameObject.tag == "zona")
+        {
+            Destroy();
+        }
+
+
+    }
+
+}   
