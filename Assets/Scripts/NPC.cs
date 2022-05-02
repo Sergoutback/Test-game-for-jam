e92fb5fc7f07d5f5c49b188cd3b3af037140924a
VladimirKirillovskiy <vladimir.kirillovski@mheducation.com>
2022-05-02 22:43:01 +0600

    added stun strike

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    void Start() {
-        //
-    }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player") {
             GetComponent<DialogueTrigger>().TriggerDialogue();
         }
-        Debug.Log("OnCollisionEnter2D");
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player") {
             GetComponent<DialogueTrigger>().EndDialogue();
-            Debug.Log("OnCollisionExit2D");
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,30 +9,8 @@ public class Player : MonoBehaviour
     public int health = 10;
     public Text healthText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // private void OnTriggerEnter2D(Collider2D collision)
-    // {
-    //     if (collision.CompareTag("BulletClone"))
-    //     {
-    //         health -= 1;
-    //         text.text = health.ToString();
-    //     }
-    // }
-
     public void takeDamage(int damage)
     {
-        Debug.Log(damage + " damage to the player!");
         health -= damage;
         healthText.text = "HP: " + health;
         if (health <= 0)
