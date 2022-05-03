064b79298303b061d0b6c659721f2da642ed03da
VladimirKirillovskiy <vladimir.kirillovski@mheducation.com>
2022-05-03 17:50:43 +0600

    prefabs and scripts for bear player

--- Assets/Scripts/ShootingBear.cs
+++ Assets/Scripts/ShootingBear.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooting : MonoBehaviour
+public class ShootingBear : MonoBehaviour
 {
-    public GameObject bullet;
+    public GameObject roar;
     public GameObject strike;
-    public Transform firePoint;
-    public float bulletSpeed = 50;
     Vector2 lookDirection;
     float lookAngle;
 
     private bool stunReady = true;
+    private bool roarReady = true;
 
     void Update()
     {
@@ -21,41 +20,39 @@ public class Shooting : MonoBehaviour
             Vector2 direction = (lookDirection - (Vector2) transform.position).normalized;
             lookAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
 
-            if (Input.GetMouseButtonDown(0))
-            {
-                GameObject bulletClone = Instantiate(bullet);
-                bulletClone.transform.position = firePoint.position;
-                bulletClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle - 180);
-
-                bulletClone.GetComponent<Rigidbody2D>().velocity = firePoint.right * bulletSpeed;
-                Destroy(bulletClone, 5);
-            }
-
-            if (Input.GetMouseButtonDown(1) && stunReady)
+            if (Input.GetMouseButtonDown(0) && stunReady)
             {
                 stunReady = false;
                 GameObject strikeClone = Instantiate(strike);
                 strikeClone.transform.position = (Vector2) transform.position + direction * 30;
                 strikeClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle + 90);
                 Destroy(strikeClone, 0.2f);
-                StartCoroutine(CoolDownCoroutine());
+                StartCoroutine(CoolDownStrikeCoroutine());
 
             }
+            if (Input.GetMouseButtonDown(1) && roarReady)
+            {
+                roarReady = false;
+                GameObject roarClone = Instantiate(roar);
+                roarClone.transform.position = (Vector2) transform.position + direction * 30;
+                roarClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle + 180);
+                Destroy(roarClone, 0.4f);
+                StartCoroutine(CoolDownRoarCoroutine());
+            }
+
         }
 
     }
 
 
-    IEnumerator CoolDownCoroutine() {
-        yield return new WaitForSeconds(1f);
+    IEnumerator CoolDownStrikeCoroutine() {
+        yield return new WaitForSeconds(0.2f);
         stunReady = true;
     }
-    
+
+    IEnumerator CoolDownRoarCoroutine() {
+        yield return new WaitForSeconds(1f);
+        roarReady = true;
+    }
 }
-    
-
-    
-
-  
